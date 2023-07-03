b13bd1e8e6c5727e2409b1a16d76d254b4f36200
enoch <enokkang@gmail.com>
2023-07-03 13:07:35 -0500

    adds feature 1 & 2

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,26 @@
 class MyExamplesController < ApplicationController
+  def show
+    #render json: { message: "hello" }
+
+    num = rand(1..3)
+    fortune = ""
+    if num == 1
+      fortune = "Buy a lottery ticket!"
+    elsif num = 2
+      fortune = "Stay Quiet!"
+    elsif num = 3
+      fortune = "Avoid Water!"
+    end
+
+    #index = 1
+    render json: { feature1: fortune,
+                   feature2: [rand(1..60), rand(1..60), rand(1..60), rand(1..60), rand(1..60), rand(1..60)],
+                   feature3: 0 }
+    #index += 1
+  end
+
+  def print_msg
+
+    #render json: {value: 0}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/my_examples", controller: "my_examples", action: "show"
 end
